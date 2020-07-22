aa635b98dfaecb017df4631a376e1a96406e500a
Gabriel <gmonteeiro@gmail.com>
2020-07-22 19:14:42 -0300

    Updates

---
+++ precache-manifest.3653936532935e35dd244beeac488cc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f27f9084fa7510206c288052bdddeb",
+    "url": "/cindor20/index.html"
+  },
+  {
+    "revision": "8a350421895ec836a050",
+    "url": "/cindor20/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "5810700b19bc8e322e2a",
+    "url": "/cindor20/static/css/main.a354bfdc.chunk.css"
+  },
+  {
+    "revision": "8a350421895ec836a050",
+    "url": "/cindor20/static/js/2.eaad3622.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cindor20/static/js/2.eaad3622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5810700b19bc8e322e2a",
+    "url": "/cindor20/static/js/main.7b60a6f6.chunk.js"
+  },
+  {
+    "revision": "59ea56d7653a1903cda8",
+    "url": "/cindor20/static/js/runtime-main.408ec473.js"
+  },
+  {
+    "revision": "31c60f0e8f99438fd28703a5ea923fce",
+    "url": "/cindor20/static/media/bg-progcientifica.31c60f0e.jpg"
+  }
+]);
